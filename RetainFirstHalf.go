95c5324379762a03bfd9d65e5ca81908b8368997
inakondu1 <inaisah66@gmail.com>
2025-10-23 02:43:50 +0100

    printfirsthalf

---
+++ RetainFirstHalf.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func RetainFirstHalf(str string) string {
+	length := len(str)
+	if length == 0 {
+		return ""
+	}
+	if length == 1 {
+		return str
+	}
+	half := length / 2
+	return str[:half]
+}
+
+func main() {
+	fmt.Println(RetainFirstHalf("abcdef"))
+}
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module ina
 
 go 1.22.2
-
-require github.com/01-edu/z01 v0.2.0 // indirect
